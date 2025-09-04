57fb3b00c7d9a4a811aa21bf134aaecc51145de7
Michael Salam <michaeldimimu@gmail.com>
2025-09-04 20:08:36 +0100

    Install and set up mongodb adapter for auth.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import { signIn, signOut } from "@/auth/auth";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google");
+        }}
+      >
+        <button type="submit">Sign in</button>
+      </form>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign out</button>
+      </form>
     </main>
   );
 }
--- auth/auth.ts
+++ auth/auth.ts
@@ -1,6 +1,9 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import client from "./lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: MongoDBAdapter(client),
   providers: [Google],
 });
