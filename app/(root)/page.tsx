dc9d420538521f8dfb312434056ce05f3ce07625
Michael Salam <michaeldimimu@gmail.com>
2025-09-08 15:01:13 +0100

    Display dummy chart in charts page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import { signOut } from "@/auth/auth";
 import getCachedSession from "@/auth/lib/getCachedSession";
 import { redirect } from "next/navigation";
+import { Bar } from "react-chartjs-2";
 
 export default async function Home() {
   const session = await getCachedSession();
@@ -8,6 +9,16 @@ export default async function Home() {
   if (!session?.user) {
     redirect("/sign-in");
   }
+
+  const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
 
   return (
     <>
