7a68f43ae2a9d28bd7ef3cb318f557a725591258
Michael Salam <michaeldimimu@gmail.com>
2025-09-15 13:39:02 +0100

    Remove loading spinner from next toploader

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
     <html lang="en">
       <SessionProvider>
         <body className={inter.variable}>
-          <NextTopLoader />
+          <NextTopLoader showSpinner={false} />
           {children}
         </body>
       </SessionProvider>
