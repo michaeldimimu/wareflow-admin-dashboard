351a026eaf02c88c26d25ad17e23961265855113
Michael Salam <michaeldimimu@gmail.com>
2025-09-15 13:28:18 +0100

    Install next-top-loader and add it to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = localFont({
   src: "./fonts/Inter-VariableFont_opsz,wght.ttf",
@@ -21,7 +22,10 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <SessionProvider>
-        <body className={inter.variable}>{children}</body>
+        <body className={inter.variable}>
+          <NextTopLoader />
+          {children}
+        </body>
       </SessionProvider>
     </html>
   );
